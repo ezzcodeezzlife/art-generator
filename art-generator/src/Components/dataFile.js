0f3aab5bf9b5c7296c73284f891c72b500ba4f2d
David Kubánek <60887837+davidkubanek@users.noreply.github.com>
2022-08-09 13:55:37 +0200

    Added word cloud suggestions into dataFile

--- art-generator/src/Components/dataFile.js
+++ art-generator/src/Components/dataFile.js
@@ -3,17 +3,63 @@
  */
 
 var Facts = {
-    funFacts: ['fact one', 'fact two']
+    funFacts: ['fact one', 'fact two'],
+
+    emotions: ['light', 'peaceful', 'calm', 'serene', 'soothing', 'relaxed',
+    'comforting', 'cosy', 'tranquil', 'quiet', 'delicate', 'subtle', 'mild', 'ethereal', 'elegant',
+    'tender', 'soft', 'light', 'bright', 'vibrant', 'dynamic', 'spirited',
+    'vivid', 'lively', 'energetic', 'colorful',
+    'joyful', 'romantic', 'expressive', 'rich', 'ecstatic', 'energized', 'zestful',
+    'passionate', 'hot', 'muted', 'bleak', 'funereal', 'somber', 'melancholic', 'mournful', 'gloomy',
+    'dismal', 'sad', 'pale, washed-out','desaturated', 'grey', 'dull', 'dreary', 'depressing', 'weary', 'tired', 'dark', 
+    'ominous', 'threatening','haunting', 'forbidding', 'gloomy', 'stormy', 'doom', 'apocalyptic','sinister', 'shadowy', 
+    'ghostly', 'unnerving', 'harrowing', 'dreadful', 'frightful', 'shocking', 'terror', 'hideous', 'ghastly', 'terrifying'],
+
+    structure_form: ['swirling','organic','flowing','turbulent','distorted','uneven','lush','chaotic','monumental','rigorous',
+    'imposing','geometric','ordered','angular','straight','rhythmic','composed','unified','harmonic','manmade','minimalist',
+    'blocks','robust','defined','ornate','delicate','neat','precise','detailed','fine','elaborate','intricate','accurate',
+    'meticulous','decorative','random','casual','spontaneous','uniform','offhand','experimental','improvisational','loose'],
+
+    looks: [ 'kaleidoscopic',
+    'psychedelic', 'saturated', 'minimalist', 'vaporwave', 'neon', 'futuristc','post-apocalyptic','gothic','fantasy','cyberpunk',
+    'cybernetic','sci-fi','steampunk','memphis','dieselpunk','industrial','afrofuturism','biopunk'],
+
+    art_styles: ['cave paintings', 'ancient egyptian mural', 'byzantine','Renaissance','Baroque','Romanticism','Impressionism','Realism','Art Nouveau','Mannerism','Roccoco',
+    'Neoclassicism','Symbolist','Art deco', 'Abstract', 'Bauhaus','Cubism', 'Fauvism', 'Dada', 'Expressionism', 'Constructivism', 'Surrealism', 'Abstract Expressionism'],
+
+    artists: ['Pablo Picasso', 'Salvador Dali', 'Keith Haring', 'Vincent van Gogh'],
+
+    //painting
+
+    painting_types: ['crayon', 'child drawing', 'acrylic on canvas', 'watercolor','oil painting','coloured pencil', 'chinese watercolor','pastels',
+    , 'airbrush','photocollage','vector art','digital art', 'pencil drawing','pencil sketch','woodcut','charcoal sketch','cartoon','animation', 'comic style','etching', 'mural',
+    'blueprint', 'botanical illustration', 'anatomical illustration','map','scientific diagram','voroni diagram','pixel art'],
+
+    content_painting: [''],
+
+    // sculpture
+    sculpture_types: ['bronze','marble','terracota','jade', 'gold', 'silver','ice carving', 'plastic','fiberglass','plant','sand','butter'],
+    
+    content_sculpture: [''],
+
+    
+
+    
+    //photography
+
+    content_photo: [''],
+
+    camera_angles: ['extreme close-up', 'close-up', 'medium-shot', 'long-shot', 'wide-shot', 'overhead view', 'low angle', 'aerial view','tilted frame'],
+
+    camera_settings: ['fast shutter speed', 'slow shutter speed', 'tilt shift photo', 'motion blur', 'telephoto lens',
+    'deep depth of field', 'macro lens', 'panoramic', 'super-wide', 'fish-eye lens', 'kodachrome', 'polaroid','double exposure','daguerrotype','disposable camera',
+    'CCTV', 'infrared','redscale','black and white'],
+
+    lighting: ['golden hour', 'blue hour','midday','overcast','sunny day', 'rainy weather', 'warm lighting',
+    'flash photography', 'studio lighting','colored lights']
 }
 
 module.exports = { Facts };
 
 
 
-// var Student = {
-//     name : "ABC",
-//     age : 18,
-//     dept : "CSE",
-//     score : 90
-// };
-// module.exports = {Student};
